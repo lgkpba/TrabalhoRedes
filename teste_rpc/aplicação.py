7580c1c50f4ee503b5ad818881a9d2f4d3389dc7
gabriel m <gabrielchagas3002@gmail.com>
2023-11-20 02:17:28 -0300

    teste rpc

---
+++ teste_rpc/aplicação.py
@@ -0,0 +1,152 @@
+
+import socket
+import time
+import json
+
+# printa funcoes disponiveis
+
+
+def printFuncoes():
+    sen = "sen(x) - retorna o valor (arredondado) do seno de x radianos (float), sendo x um número real (float)\n"
+    cos = "cos(x) - retorna o valor (arredondado) do cosseno de x radianos (float), sendo x um número real (float)\n"
+    tan = "tan(x) - retorna a valor (arredondado) da tangente de x radianos (flaot), sendo x número real (float)\n\n"
+    info = "digite 'funcoes()' para ver as funções disponíveis e 'quit()', para sair do programa"
+    print("\n\n" + "     RPC - FUNÇÕES TRIGONOMÉTRICAS     ".center(len(cos), "#") + "\n")
+    print("Funções Disponíveis: ")
+    print(sen+cos+tan+info)
+
+# verifica se há os parentesis ou chaves do input estão fechados
+
+
+def loopCheckEntrada(en):
+    count = 0
+    for char in en:
+        if char == '(' or char == "[":
+            count += 1
+        elif char == ')' or char == "]":
+            count -= 1
+        if count < 0:
+            return False
+    return (count == 0)
+
+# verifica a entrada, tratando os casos de inputs inválidos
+
+
+def verificaEntrada(en):
+    if (en == "q" or en == "Q" or en == "F" or en == "f"):
+        return en
+
+    elif (en == "" or en.isspace()):
+        return False
+
+    elif ((not en[0].isalpha)):
+        print("NameError: Função não reconhecida")
+        return False
+
+    elif ((en.split("(")[0] not in funcoes)):
+        print("NameError: Função não reconhecida")
+        return False
+
+    elif (not loopCheckEntrada(en)):
+        print("NameError: Função não reconhecida")
+        return False
+
+    return en
+
+# funçoes stub - todas tentam se comunicar com o servidor atraves de pacotes json
+
+# funçao stub seno
+
+
+def sen(x: float) -> float:
+    # cria e configura socket
+    socketCliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socketCliente.connect(('localhost', 4900))
+    # cria requisição
+    rqst = {
+        "nome": "sen",
+        "parametro": x
+    }
+    # converte requisição para json e converte para bytes
+    rqstBytes = json.dumps(rqst).encode("utf-8")
+
+    socketCliente.send(rqstBytes)  # envia requisição
+    respBytes = socketCliente.recv(256).decode(
+        "utf-8")  # recebe resposta e converte
+    resp = json.loads(respBytes)["retorno"]  # pega retorno da funcao
+    socketCliente.close()
+    return resp
+
+# funçao stub cosseno - mesma coisa do seno
+
+
+def cos(x: float) -> float:
+    # cria e configura socket
+    socketCliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socketCliente.connect(('localhost', 4900))
+    # cria requisição
+    rqst = {
+        "nome": "sen",
+        "parametro": x
+    }
+    # converte requisição para json e converte para bytes
+    rqstBytes = json.dumps(rqst).encode("utf-8")
+
+    socketCliente.send(rqstBytes)  # envia requisição
+    respBytes = socketCliente.recv(256).decode(
+        "utf-8")  # recebe resposta e converte
+    resp = json.loads(respBytes)["retorno"]  # pega retorno da funcao
+    socketCliente.close()
+    return resp
+
+# funcao stub tangente
+
+
+def tan(x: float) -> float:
+    # cria e configura socket
+    socketCliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socketCliente.connect(('localhost', 4900))
+    # cria requisição
+    rqst = {
+        "nome": "sen",
+        "parametro": x
+    }
+    # converte requisição para json e converte para bytes
+    rqstBytes = json.dumps(rqst).encode("utf-8")
+
+    socketCliente.send(rqstBytes)  # envia requisição
+    respBytes = socketCliente.recv(256).decode(
+        "utf-8")  # recebe resposta e converte
+    resp = json.loads(respBytes)["retorno"]  # pega retorno da funcao
+    socketCliente.close()
+    return resp
+
+
+# variavel global lista com as funções possiveis
+funcoes = ["sen", "cos", "tan", "quit", "funcoes"]
+
+# funcao principal
+
+
+def main():
+    printFuncoes()
+    print(">>> ", end="")
+    while True:  # laço principal do programa
+        en = verificaEntrada(input())
+        if (not en):
+            pass
+        elif (en == "q" or en == "Q" or en[0:4] == "quit"):
+            break
+        elif (en == "f" or en == "F" or en[0:8] == "funcoes"):
+            printFuncoes()
+        else:  # bloco onde a aplicação tenta executar o input já tratado
+            try:
+                print(eval(en))  # eval executa as funções stubs
+            except Exception as e:
+                print(f"Error: {e}")
+        print(">>> ", end="")
+
+
+# inicia função princiapl
+if __name__ == "__main__":
+    main()
---
+++ teste_rpc/servidor.py
@@ -0,0 +1,90 @@
+
+import socket
+import json
+import math
+
+# funcao stub seno
+
+
+def sen(x: float) -> str:
+    return math.sin(x)
+
+# funcao stub cosseno
+
+
+def cos(x: float) -> str:
+    return math.cos(x)
+
+# funcao stub tangente
+
+
+def tan(x: float) -> str:
+    return math.tan(x)
+
+
+# variavel global dicionario com as funcoes
+funcoes = {"sen": sen,
+           "cos": cos,
+           "tan": tan
+           }
+
+
+class Servidor:
+    # localhost se comunica com a propria maquina, mudar para o endereço ip real na rede
+    def __init__(self, endereco_servidor='localhost', porta_servidor=4900, max_conexoes=1):
+        # cria e configura socket
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # endereço ip 'localhost' e porta 4900
+        self.socket.bind((endereco_servidor, porta_servidor))
+        # sem conexões simultâneas, servidor simples e humilde que aceita apenas um única conexão
+        self.socket.listen(max_conexoes)
+
+        self.loopServidor()
+
+    def loopServidor(self):
+        while True:
+            timeout = 30
+            self.socket.settimeout(timeout)  # define espera para conexão
+            try:
+                # servidor aceita conexão - treeway handshake
+                (socketParaCliente, enderecoDoCliente) = self.socket.accept()
+                pacote = socketParaCliente.recv(256)  # servidor recebe pacote
+            except TimeoutError as e:  # não houve conexão
+                print(
+                    f"{timeout} segundos se passaram e não houve tentiva de conexão")
+                print(f"SERVIDOR ENCERRADO")
+                break
+            except Exception as e:  # se der erro, deu erro
+                print(e)
+                print(f"SERVIDOR ENCERRADO")
+                break
+
+            # decodifica pacote json em dicionario
+            rqst = json.loads(pacote.decode("utf-8"))
+            print(
+                f"Servidor recebeu do cliente {enderecoDoCliente} a mensagem: {rqst}")
+            respBytes = self.handler(rqst).encode("utf-8")  # processa resposta
+            print(
+                f"Servidor enviou para cliente cliente {enderecoDoCliente} a mensagem: {respBytes.decode('utf-8')}")
+            socketParaCliente.send(respBytes)  # envia resposta já em bytes
+            break  # !!!!!! Servidor temrmina após enviar resposta, arrumar dps
+
+        self.socket.close()  # fecha socket após temrinar loop
+
+    def handler(self, rqst):
+        # pega nome e parametro do request
+        (nome, param) = (rqst["nome"], rqst["parametro"])
+        try:
+            rtrn = funcoes[nome](param)  # executa funcao stub localmente
+        except Exception as e:  # captura erro
+            rtrn = f"Error: {str(e)}"
+        resp = {
+            "chamada": f"{nome}({param})",
+            "retorno": rtrn
+        }
+        return json.dumps(resp)  # retorna resposta já convertida em json
+
+
+server = Servidor()
+print("SERVIDOR ENCERRADO")
+del server  # literalmente apaga o servidor da memória
